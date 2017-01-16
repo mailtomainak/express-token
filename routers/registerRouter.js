981e16dfbe34223d18bf5aefaf0ab7a3a98b30d0
mainak <mailtomainak@gmail.com>
2017-01-16 23:12:09 +0530

    Add session support

--- routers/registerRouter.js
+++ routers/registerRouter.js
@@ -15,9 +15,22 @@ registerRouter.get('/',function(req,res,next){
 
 registerRouter.post('/',function(req,res,next){	
 	userController.saveUser(req.body,function(err,data){
-		if(err) res.send(err);
-		res.send(data);
+		if(err) {
+			res.send(err);
+		}
+		else{
+			req.session.email=data.email;
+			res.redirect(303,'/register/success');		}
+		
 	});
+});
+
+
+registerRouter.get('/success',function(req,res,next){
+	// const email = req.session.email;
+	// console.log(req.session);
+	//req.session.email = null;
+	res.render('register-success');
 });
 
 module.exports = registerRouter;
